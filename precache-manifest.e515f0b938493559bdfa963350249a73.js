44eb689ba63cbd683fcdfb1f43ff1c56feaca9f5
Pratyush Praveen <pratyush1296@gmail.com>
2021-02-12 13:42:18 +0530

    Updates

---
+++ precache-manifest.e515f0b938493559bdfa963350249a73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb50d75aad5fa38750e0fb7dc1bfd8a",
+    "url": "/Keeper-App-React/index.html"
+  },
+  {
+    "revision": "39060614b581c1e34ecc",
+    "url": "/Keeper-App-React/static/js/2.2d80bb48.chunk.js"
+  },
+  {
+    "revision": "ccddd6543e8c286c8aa8",
+    "url": "/Keeper-App-React/static/js/main.314b6d6e.chunk.js"
+  },
+  {
+    "revision": "c2d1d8540df61f608b07",
+    "url": "/Keeper-App-React/static/js/runtime-main.1957baba.js"
+  }
+]);
--- src/component/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function heading(){
-   return <header>
-   <h1>Keeper</h1>
-   </header>
-    }
-    
-    export default heading;
